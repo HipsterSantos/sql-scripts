1ea3ba9e740a6ee24947f62d57c038263dfff921
HipsterSantos <santoscampos269@gmail.com>
2021-03-29 02:50:19 +0100

    adding previlegies to user

--- create-use.sql
+++ create-use.sql
@@ -5,3 +5,13 @@ CREATE USER 'FREet'@'demo.demo.co' IDENTIFIED BY 'stream';
 
 CREATE USER 'max'@'%' IDENTIFIED BY 'mistitc';
 CREATE USER 'MAXXAXX'@'%' IDENTIFIED BY 'noone';
+
+-- these are wildcars % and _ it really means *
+CREATE USER 'juese'@'%.sname.io' IDENTIFIED BY 'none';
+CREATE USER 'JOE'@'192.168.128.3' IDENTIFIED BY 'water';
+
+--Granting previlegies to users
+
+GRANT create,insert,update,alter(previlegies) *.*(colums)
+ON what(database.table|view|index|function|storedprocedure)
+TO account IDENTIFIED BY 'password'
