b1b20e96b92fc5929a21aea5133fe99c100e19cb
HipsterSantos <santoscampos269@gmail.com>
2021-03-29 02:41:51 +0100

    adding user syntax

---
+++ create-use.sql
@@ -0,0 +1,7 @@
+-- CREATE USER account [identified by 'password']  this is the syntax to create a user on sql languages
+CREATE USER 'boris'@'localhost' IDENTIFIED BY 'fronst'
+CREATE USER 'FREet'@'demo.demo.co' IDENTIFIED BY 'stream';
+-- By doing in this way we're specifying the location where user can accsess ,how do I anble user ,anywhere as location
+
+CREATE USER 'max'@'%' IDENTIFIED BY 'mistitc';
+CREATE USER 'MAXXAXX'@'%' IDENTIFIED BY 'noone';
