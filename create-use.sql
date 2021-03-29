550c7040bcfd139065e21d0964ff2c4731047fc9
HipsterSantos <santoscampos269@gmail.com>
2021-03-29 03:03:54 +0100

    create database and alter it

--- create-use.sql
+++ create-use.sql
@@ -14,4 +14,32 @@ CREATE USER 'JOE'@'192.168.128.3' IDENTIFIED BY 'water';
 
 GRANT create,insert,update,alter(previlegies) *.*(colums)
 ON what(database.table|view|index|function|storedprocedure)
-TO account IDENTIFIED BY 'password'
+TO account
+IDENTIFIED BY 'password'
+
+CREATE USER 'ethel1'@'localhost' IDENTIFIED BY 'coffee';
+GRANT ALL ON *.* TO 'ethel'@'localhost' WITH GRANT OPTION;
+GRANT RELOAD ON database_name.table  TO 'noone'@'local.local.io';
+
+
+CREATE USER 'jennie'@'%' IDENTIFIED BY 'carbon'
+GRANT SELECT,INSERT,DELETE,UPDATE ON sampdb.* TO 'jennie'@'%';
+--granting access for individuals tables and colums
+CREATE USER 'assistant'@'localhost' IDENTIFIED BY 'officalhelp';
+GRANT SELECT,UPDATE(expiration,street,city,state,zip) ON sampdb.member TO 'assistant'@'localhost';
+
+
+
+====  CREATING A DATABASE ====
+
+CREATE DATABASE IF NOT EXISTS db_name
+CHARSET utf8
+DEFAULT COLLATION utf8_general_ci;
+
+SHOW CREAE DATABASE;
+
+=== DROP A DATABSE ===
+DROP DATABASE db_name;
+
+===ALTERING  DATABASE
+ALTER DATABASE [db_name] [CHARACTER SET charset] [COLLATE collation];
