1950ab3101e32c89a7e5579bb35ff4639e8baa6d
HipsterSantos <santoscampos269@gmail.com>
2021-04-01 06:09:28 +0100

    Conditional control statement

--- pl/shinanigans.sql
+++ pl/shinanigans.sql
@@ -69,6 +69,27 @@ END;
 
 
 
+DECLARE 
+    salary NUMBER := 20000; 
+    emplyee_id NUMBER := 36325;
+
+    PROCEDURE give_bonus(emp_id IN NUMBER , bonus_amt IN NUMBER )IS 
+    BEGIN 
+        DBMS_OUTPUT.PUT_LINE(emp_id);
+        DBMS_OUTPUT.PUT_LINE(bonus_amt);
+    END;
+
+    BEGIN 
+        give_bonus(employee_id,
+             CASE 
+             WHEN salary >= 1000 AND salary <=  20000 THEN 1500
+             WHEN salary > 20000 AND  salary <= 40000 THEN 1000
+             WHEN  salary > 40000 THEN 500
+             ELSE 0
+        END;)
+
+
+
 
 -- Sequential Contol Statements
 
