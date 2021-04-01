3cc4df85dc3af3a18641a3142333d9776e3be5b0
HipsterSantos <santoscampos269@gmail.com>
2021-04-01 05:40:28 +0100

    Conditonal control statement case when else

--- pl/shinanigans.sql
+++ pl/shinanigans.sql
@@ -15,5 +15,23 @@ ELSE
   makeNothing()
 END IF 
 
+CASE region_id
+    WHEN 'NE' THEN 
+        mgr_name := 'Miner';
+    WHEN 'SE' THEN
+        mgr_name := 'KOOI';
+    ELSE mgr_name := 'LANE'; --THIS IS THE DEFAULT BLOCK
+END CASE; -- THIS IS THE ENDING OF THIS CASE 
+
+CASE cod_status
+    WHEN 0 THEN 
+        status:='Married'
+    WHEN 1 THEN
+        status:='Not Married'
+    ELSE 
+        status :='Not Married'
+END CASE;
+
+
 -- Sequential Contol Statements
 
