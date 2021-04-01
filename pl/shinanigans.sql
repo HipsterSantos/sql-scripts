03127298048d45ff56e54857c72d74e606f12df4
HipsterSantos <santoscampos269@gmail.com>
2021-04-01 05:33:32 +0100

    Conditional control statement

---
+++ pl/shinanigans.sql
@@ -0,0 +1,19 @@
+-- Conditional Control statements 
+
+IF hasValue() THEN 
+    makesomething(0);
+ELSE 
+    makeNohing();
+END IF;
+
+
+IF hasValue() THEN 
+    makesomething();
+ELSE IF not(hasValue) AND 3=3 THEN
+    makesomething();
+ELSE 
+  makeNothing()
+END IF 
+
+-- Sequential Contol Statements
+
