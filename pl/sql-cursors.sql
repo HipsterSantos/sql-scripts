be3a53c00ec018aecd89570ecf7c3593e6b9a061
HipsterSantos <santoscampos269@gmail.com>
2021-04-04 07:13:45 +0100

    fetching cursor ,adding declaration ,and closing cursor,

--- pl/sql-cursors.sql
+++ pl/sql-cursors.sql
@@ -53,3 +53,22 @@ Closing the Cursor
 Closing the cursor means releasing the allocated memory. For example, we will close the above-opened cursor as follows −
 
 CLOSE c_customers;
+
+//challenge 
+
+DECLARE 
+    c_id customers.id%type;
+    c_name customers.name%type;
+    c_addr customers.address%type;
+    CURSOR c_customers is 
+        SELECT id,name,address FROM customers;
+
+BEGIN 
+    OPEN c_customers;
+    LOOP
+    FETCH c_customers into c_id,c_name,c_addr
+        EXIT WHEN c_customers%notfound;
+        dbms_output.put_line(c_id || ' ' || c_name || ' ' || c_addr);
+    END LOOP;
+    CLOSE c_customers;
+END;
