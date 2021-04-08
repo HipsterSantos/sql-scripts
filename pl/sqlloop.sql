932ba9328509878e32a11c64cb4fdaecbf68c402
HipsterSantos <santoscampos269@gmail.com>
2021-04-08 06:12:32 +0100

    foor loop

--- pl/sqlloop.sql
+++ pl/sqlloop.sql
@@ -2,3 +2,10 @@ LOOP
 
     executable_statetment
 END LOOP ;
+
+LOOP 
+    FETCH company_cur INTO copany_rec;
+    EXIT WHEN company_cur%ROWCOUNT > 5 OR 
+    company_cur%NOTFOUND;
+    proces_company(company_cur);
+    END LOOP;
